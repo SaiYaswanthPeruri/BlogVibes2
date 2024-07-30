47138cb1001814fce0996f17ce134052f9b72010
SaiYaswanthPeruri <yaswanthperuri123@gamil.com>
2024-07-30 20:34:28 +0530

    updated

---
+++ src/components/Logo.jsx
@@ -0,0 +1,188 @@
+import React from 'react';
+
+function Logo({ width = '47px' }) {
+  return (
+    <div style={{ width }}>
+      <svg
+        version="1.1"
+        id="icons"
+        xmlns="http://www.w3.org/2000/svg"
+        x="0"
+        y="0"
+        viewBox="0 0 64 64"
+        style={{ enableBackground: "new 0 0 64 64" }}
+        xmlSpace="preserve"
+      >
+        <style>
+          {`.st0{display:none}
+            .st1{fill:#36d7b7}.st1,.st2,.st3{display:inline}
+            .st2{fill:none;stroke:#414042;stroke-miterlimit:10}
+            .st3{fill:#f5ab35}
+            .st4{fill:#f7bc5d}.st4,.st5{display:inline}
+            .st6{fill:#414042}
+            .st7{fill:#36d7b7}
+            .st9{fill:#f1f2f2}.st10,.st11,.st9{display:inline}
+            .st10{fill:none;stroke:#414042;stroke-linecap:round;stroke-miterlimit:10}
+            .st11{fill:#414042}
+            .st12,.st14,.st15{display:inline;fill:#e6e7e8}
+            .st14,.st15{fill:#d1d3d4}.st15{fill:#fff}
+            .st16{fill:none;stroke:#414042;stroke-miterlimit:10}
+            .st17{fill:#fff}
+            .st18{fill:#f5ab35}
+            .st19{fill:#f9cd86}
+            .st20{fill:#29a189}
+            .st21{fill:#f7bc5d}
+            .st22{display:inline;fill:#29a189}
+            .st23{fill:#e6e7e8}
+            .st26{fill:#5edfc5}
+            .st27,.st28{fill:none;stroke-miterlimit:10}
+            .st27{display:inline;stroke:#414042;stroke-dasharray:1}
+            .st28{stroke:#36d7b7}
+            .st31,.st32{display:inline}
+            .st31{fill:#bcbec0}
+            .st32{stroke:#f5ab35}.st32,.st33,.st34{fill:none;stroke-miterlimit:10}
+            .st33{display:inline;stroke:#36d7b7}
+            .st34{stroke:#414042;stroke-linecap:round}
+            .st35{display:inline;fill:#5edfc5}
+            .st38{fill:none;stroke:#fff;stroke-miterlimit:10}
+            .st39,.st42{display:inline}
+            .st39{fill:none;stroke:#fff;stroke-miterlimit:10}
+            .st42{fill:#86e7d4}`}
+        </style>
+        <g id="XMLID_129_">
+          <path
+            id="XMLID_805_"
+            className="st7"
+            d="M14.9 23v15.8h2V44.1c0 .3.3.4.5.2l6.6-5.4h20V23H14.9z"
+          />
+          <path
+            id="XMLID_113_"
+            className="st16"
+            d="M14.9 38.8h2V44.1c0 .3.3.4.5.2l6.6-5.4h20"
+          />
+          <g id="XMLID_407_">
+            <path
+              id="XMLID_414_"
+              className="st17"
+              d="M35.3 34.2 47 22.6l-1.7-1.7-11.7 11.6-.7 2.3s0 .1.1.1l2.3-.7z"
+            />
+            <path
+              id="XMLID_413_"
+              transform="rotate(134.999 40.67 27.153)"
+              className="st18"
+              d="M32.7 25.9h15.8v2.4H32.7z"
+            />
+            <path
+              id="XMLID_412_"
+              transform="rotate(134.999 40.243 26.726)"
+              className="st19"
+              d="M32.3 26.1h15.8v1.2H32.3z"
+            />
+            <path
+              id="XMLID_411_"
+              transform="rotate(134.999 47.126 20.698)"
+              className="st17"
+              d="M46.1 19.2h2v3h-2z"
+            />
+            <path
+              id="XMLID_410_"
+              className="st16"
+              d="M36.4 33.6 49.1 21l-2.2-2.2-12.7 12.6-1.3 3.4c0 .1 0 .1.1.1l3.4-1.3z"
+            />
+            <path id="XMLID_409_" className="st16" d="m45.2 20.5 2.2 2.2" />
+            <path id="XMLID_408_" className="st16" d="m34.3 32 1.5 1.5" />
+          </g>
+          <path
+            id="XMLID_110_"
+            className="st16"
+            d="M20.6 26.9h16.1"
+          />
+          <path
+            id="XMLID_109_"
+            className="st16"
+            d="M20.6 30.9h12"
+          />
+          <path
+            id="XMLID_111_"
+            className="st16"
+            d="M20.6 34.9h10.8"
+          />
+          <g id="XMLID_166_">
+            <path
+              id="XMLID_168_"
+              className="st16"
+              d="m29 40.8 2.4 2.4"
+            />
+            <path
+              id="XMLID_167_"
+              className="st16"
+              d="m29 43.2 2.4-2.4"
+            />
+          </g>
+          <g id="XMLID_120_">
+            <path
+              id="XMLID_122_"
+              className="st16"
+              d="m28.3 18.5 2.3 2.4"
+            />
+            <path
+              id="XMLID_121_"
+              className="st16"
+              d="m28.3 20.9 2.3-2.4"
+            />
+          </g>
+          <g id="XMLID_123_">
+            <path
+              id="XMLID_125_"
+              className="st16"
+              d="M19.1 14.2 22 17"
+            />
+            <path
+              id="XMLID_124_"
+              className="st16"
+              d="m19.1 17 2.9-2.8"
+            />
+          </g>
+          <g id="XMLID_126_">
+            <path
+              id="XMLID_128_"
+              className="st16"
+              d="m35.8 11.3 2.9 2.9"
+            />
+            <path
+              id="XMLID_127_"
+              className="st16"
+              d="m35.8 14.2 2.9-2.9"
+            />
+          </g>
+          <g id="XMLID_112_">
+            <path
+              id="XMLID_115_"
+              className="st16"
+              d="m38.6 45 3.1 3.1"
+            />
+            <path
+              id="XMLID_114_"
+              className="st16"
+              d="m38.6 48.1 3.1-3.1"
+            />
+          </g>
+          <g id="XMLID_116_">
+            <path
+              id="XMLID_118_"
+              className="st16"
+              d="m25 49 3.7 3.7"
+            />
+            <path
+              id="XMLID_117_"
+              className="st16"
+              d="m25 52.7 3.7-3.7"
+            />
+          </g>
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+export default Logo;
